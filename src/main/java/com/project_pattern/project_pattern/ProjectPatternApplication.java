67708d5427f745940c2c8b8db1b4eafcbec70394
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2022-06-17 22:01:05 -0300

    init

---
+++ src/main/java/com/project_pattern/project_pattern/ProjectPatternApplication.java
@@ -0,0 +1,13 @@
+package com.project_pattern.project_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPatternApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project_pattern/project_pattern/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.project_pattern.project_pattern;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectPatternApplication.class);
+	}
+
+}
